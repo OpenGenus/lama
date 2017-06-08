2803048475c8ad23b83c71d47dc7689c90a89545
Aditya Chatterjee <aditianhacker@gmail.com>
2017-06-08 22:50:04 +0530

    rearrangement :house:

--- code/main.py
+++ code/main.py
@@ -17,7 +17,7 @@ def ask():
 	if os.path.isfile("bot_brain.brn"):
 	    kernel.bootstrap(brainFile = "bot_brain.brn")
 	else:
-	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/startup.xml"), commands = "load aiml b")
+	    kernel.bootstrap(learnFiles = os.path.abspath("brain/aiml/startup.xml"), commands = "load aiml b")
 	    kernel.saveBrain("bot_brain.brn")
 
 	while True:
